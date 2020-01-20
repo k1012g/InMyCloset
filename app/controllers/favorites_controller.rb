4c418f3548c5494a627544bbdf708f1365fe9dca
k1012g <kuri.ladybaby1012@gmail.com>
2020-01-20 11:46:30 +0000

    favorite ajax done

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,11 +5,13 @@ class FavoritesController < ApplicationController
 
   def create
   	Favorite.create(user_id: current_user.id, cloth_id: params[:cloth_id])
-  	redirect_back(fallback_location: clothes_path)
+  	# redirect_back(fallback_location: clothes_path)
+    @clothes = Cloth.find(params[:cloth_id])
   end
 
   def destroy
   	Favorite.find_by(user_id: current_user.id, cloth_id: params[:cloth_id]).destroy
-  	redirect_back(fallback_location: clothes_path)
+  	# redirect_back(fallback_location: clothes_path)
+    @clothes = Cloth.find(params[:cloth_id])
   end
 end
