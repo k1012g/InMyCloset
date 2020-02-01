d4593096f1e382e699d8db207848a55e5a3b38e7
k1012g <kuri.ladybaby1012@gmail.com>
2020-02-01 04:38:46 +0000

    detail modified

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,21 +1,31 @@
 class SearchesController < ApplicationController
   def search
   	@users = User.all
-  	@clothes = Cloth.all
-  	@categories = Category.all
   	@input = params[:input]
   	@sort = params[:sort]
   	@results = searching(@input, @sort)
   end
 
   private
+
   def searching(input, sort)
-  	if sort == "User"
-  		User.where('name LIKE ?', '%'+input+'%')
-  	elsif sort == "Brand"
-  		Cloth.where('brand LIKE ?', '%'+input+'%')
-  	elsif sort == "Category"
-  		Category.where('category LIKE ?', '%'+input+'%')
-  	end
+    if input == ""
+      if sort == "User"
+        User.all
+      elsif sort == "Brand"
+        Cloth.all
+      elsif sort == "Category"
+        Category.all
+      end
+    else
+      if sort == "User"
+        User.where('name LIKE ?', '%'+input+'%')
+      elsif sort == "Brand"
+        Cloth.where('brand LIKE ?', '%'+input+'%')
+      elsif sort == "Category"
+        Category.where('category LIKE ?', '%'+input+'%')
+      end
+    end
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,9 @@ class UsersController < ApplicationController
 
   def update
   	@user = User.find(params[:id])
+    if params[:delete] == "yes"
+      @user.profile_image = nil
+    end
   	if @user.update(user_params)
   		redirect_to user_path(@user.id)
   	else
